50bb42962889e0da547ee66e539e12d329bf9e90
Natsume <chenkai@ailieyun.com>
2023-03-02 14:25:44 +0800

    添加工具方法

---
+++ lib/utils/gyk_util.dart
@@ -0,0 +1,2 @@
+export './system_util.dart';
+export './date_time_util.dart';
---
+++ lib/utils/system_util.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/foundation.dart';
+
+class SystemUtil {
+  static TargetPlatform targetPlatform = defaultTargetPlatform;
+}
