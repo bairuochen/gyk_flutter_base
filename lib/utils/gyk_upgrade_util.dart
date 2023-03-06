f134e4c228e1c7ff999fcb4a9b8f880f6f61ce3b
Natsume <chenkai@ailieyun.com>
2023-03-06 18:17:19 +0800

    app 升级组件自定义

--- lib/utils/gyk_upgrade_util.dart
+++ lib/utils/gyk_upgrade_util.dart
@@ -1,6 +1,3 @@
-import 'dart:io';
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter_app_update/azhon_app_update.dart';
 import 'package:flutter_app_update/update_model.dart';
 import 'package:gyk_flutter_base/models/app/gyk_app_info.dart';
@@ -10,34 +7,17 @@ class GYKUpgradeUtil {
 
   /// 升级
   static Future<void> appUpgrade(GYKAppInfo gykAppInfo) {
-    UpdateModel updateModel;
-    if (Platform.isAndroid) {
-       updateModel = UpdateModel(
-          gykAppInfo.downloadUrl,
-          "${gykAppInfo.packageName}.apk",
-          'ic_launcher',
-          convertToContent(gykAppInfo.content),
-          showNewerToast: true,
-          apkVersionCode: gykAppInfo.apkVersionCode,
-          apkVersionName: "V${gykAppInfo.apkVersionName}",
-          forcedUpgrade: gykAppInfo.forceUpgrade
-      );
-    } else if (Platform.isIOS) {
-      updateModel = UpdateModel(
-          gykAppInfo.downloadUrl,
-          "${gykAppInfo.packageName}.apk",
-          'ic_launcher',
-          convertToContent(gykAppInfo.content),
-          showNewerToast: true,
-          apkVersionCode: gykAppInfo.apkVersionCode,
-          apkVersionName: "V${gykAppInfo.apkVersionName}",
-          forcedUpgrade: gykAppInfo.forceUpgrade,
-          iOSUrl: gykAppInfo.iosUrl
-      );
-    } else {
-      return Future(() => debugPrint("不支持"));
-    }
-
+    UpdateModel updateModel = UpdateModel(
+        gykAppInfo.downloadUrl,
+        "${gykAppInfo.packageName}.apk",
+        'ic_launcher',
+        convertToContent(gykAppInfo.content),
+        showNewerToast: true,
+        apkVersionCode: gykAppInfo.apkVersionCode,
+        apkVersionName: "V${gykAppInfo.apkVersionName}",
+        iOSUrl: gykAppInfo.iosUrl,
+        forcedUpgrade: gykAppInfo.forceUpgrade
+    );
     return AzhonAppUpdate.update(updateModel);
   }
 
