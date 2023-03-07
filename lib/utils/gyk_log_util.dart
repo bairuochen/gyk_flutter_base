29622aa7dfd7dcb93962aebd19984bad88f510d8
Natsume <chenkai@ailieyun.com>
2023-03-07 14:58:24 +0800

    app 日志工具类

--- lib/utils/gyk_log_util.dart
+++ lib/utils/gyk_log_util.dart
@@ -5,7 +5,16 @@ import 'package:logger/logger.dart';
 /// 日志工具类
 class GYKLogUtil {
 
-   static final Logger logger = Logger();
+   static final Logger logger = Logger(
+     printer: PrettyPrinter(
+         methodCount: 2, // number of method calls to be displayed
+         errorMethodCount: 8, // number of method calls if stacktrace is provided
+         lineLength: 120, // width of the output
+         colors: true, // Colorful log messages
+         printEmojis: true, // Print an emoji for each log message
+         printTime: false // Should each log print contain a timestamp
+     ),
+   );
 
    /// 信息
    static void logInfo(dynamic msg) {
